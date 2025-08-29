4734835bc9e888f60d15d65768fa457e4adf84b7
Kelson Neto <kelsonneto674@gmail.com>
2025-08-29 18:57:38 +0100

    finish exercise 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    let sum = 0;
+    for (let i = Math.min(a,b); i <= Math.max(a,b); i++){
+        sum += i;
+    }
+    return sum
 };
+
+console.log(sumAll(0,4))
 
 // Do not edit below this line
 module.exports = sumAll;
