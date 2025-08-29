88ea9a8b5680310cac2af5c9ae59b0e0092eec0b
Kelson Neto <kelsonneto674@gmail.com>
2025-08-29 18:17:56 +0100

    finish exercise 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
+const reverseString = function(any) {
+    return any.split("").reverse().join("");
 
 };
+
+console.log(reverseString ("asa"))
 
 // Do not edit below this line
 module.exports = reverseString;
