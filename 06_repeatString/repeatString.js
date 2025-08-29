98c338d61b361debba6963300fcffee29d55d087
Kelson Neto <kelsonneto674@gmail.com>
2025-08-29 18:07:39 +0100

    finished exercise 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(any,num) {
+  let repeatedString = "";
+  for (let i = 0; i < num; i++){
+    repeatedString += any;
+  }
+  return repeatedString  
 };
+
+console.log(repeatString("hey", 5));
 
 // Do not edit below this line
 module.exports = repeatString;
